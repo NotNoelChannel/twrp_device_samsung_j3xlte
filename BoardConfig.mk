991f851750eede06465871f21ae3475b7c3f4a3c
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2022-07-05 12:49:07 +0200

    Get ready for SHRP

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,3 +62,7 @@ TW_MTP_DEVICE := "/dev/mtp_usb"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/20200000.usb/gadget/lun%d/file"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
+############################################
+# shrp
+SHRP_EXPRESS := true
+SHRP_LITE := true
