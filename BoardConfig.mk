57188010665409476808ccb8a63d54c5dc16e50f
Tri <triplaysyt@gmail.com>
2022-07-06 13:42:52 +0300

    j3x: Add AGPL License

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,18 @@
+# Copyright (C) 2022 NotNoelChannel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 TARGET_OTA_ASSERT_DEVICE := j3xnlte,j3xnltexx,j2xlte,j2xltedd,j3xlte
 
 # Bootloader
@@ -69,4 +84,3 @@ SHRP_LITE := true
 SHRP_MAINTAINER := notnoelchannel
 SHRP_EXTERNAL := /external_sd
 SHRP_OTG := /usb_otg
-
