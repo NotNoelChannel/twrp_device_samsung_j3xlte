5bfd960a4e620e8e397c378da7d4a4b09ac1cf28
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2023-03-18 15:12:57 +0100

    Update omni_j3xlte.mk

--- omni_j3xlte.mk
+++ omni_j3xlte.mk
@@ -19,6 +19,9 @@
 # Get the prebuilt list of APNs
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
