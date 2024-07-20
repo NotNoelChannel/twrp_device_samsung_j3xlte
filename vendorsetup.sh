5d3848c3582365cfdfcc9d500c985a07273b2d75
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2024-07-20 16:42:33 +0200

    oops

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,1 @@
-sudo git clone --depth 1 --branch android-7.1.2_r37 --single-branch https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8 /opt/toolchains/arm-eabi-4.8/
-
 add_lunch_combo omni_j3xlte-eng
